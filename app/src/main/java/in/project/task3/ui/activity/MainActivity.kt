d806c0fb98fcc5d14c2f28d212f212607029d2b3
Debashish Kundu <debashish.joy@gmail.com>
2022-07-22 19:55:03 +0530

    successfully created ui

--- app/src/main/java/in/project/task3/ui/activity/MainActivity.kt
+++ app/src/main/java/in/project/task3/ui/activity/MainActivity.kt
@@ -1,16 +1,30 @@
 package `in`.project.task3.ui.activity
 
-import `in`.project.task3.R
 import `in`.project.task3.databinding.ActivityMainBinding
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
+	private var isClicked = false
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+		binding.apply {
+			loveTheApp.setOnClickListener {
+				isClicked = !isClicked
+				if (isClicked) {
+					rate.show()
+					share.show()
+					info.show()
+				} else {
+					rate.hide()
+					share.hide()
+					info.hide()
+				}
+			}
+		}
 	}
 }
